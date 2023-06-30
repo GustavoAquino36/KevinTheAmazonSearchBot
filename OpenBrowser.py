e11723cc5b7814f8c7313155c799d84a8f3d481d
Lucas Jansen Gorges <lucasjnsgorges@gmail.com>
2023-06-30 20:55:36 -0300

    fix:Webdriver-Service

--- OpenBrowser.py
+++ OpenBrowser.py
@@ -6,13 +6,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 def OpenBrowser (driverpath, site):
   try:
       options = Options()
+
       options.add_argument("--log-level=2")
       options.add_argument("--start-maximized")
       options.add_experimental_option("detach", True)
-      service = Service(driverpath)
-      service.start()
-      driver = webdriver.Chrome(service=service(ChromeDriverManager.install()), options=options)
+
+      driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
       driver.get(site)
+      return driver
   except Exception as expt:
     print(f"Erro: {expt}")
-  return driver
