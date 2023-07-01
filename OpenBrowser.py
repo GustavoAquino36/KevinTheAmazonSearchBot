ea2e25bebef1b330e6b54c1da68c87e680d64d17
Lucas Jansen Gorges <lucasjnsgorges@gmail.com>
2023-07-01 13:12:25 -0300

    Removing:chromedriver-path

--- OpenBrowser.py
+++ OpenBrowser.py
@@ -3,14 +3,12 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-def OpenBrowser (driverpath, site):
+def OpenBrowser (site):
   try:
       options = Options()
-
       options.add_argument("--log-level=2")
       options.add_argument("--start-maximized")
       options.add_experimental_option("detach", True)
-
       driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
       driver.get(site)
       return driver
