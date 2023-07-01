a7cf80aeed64406099d47ff4058b9432884cbe12
GustavoAquino36 <gustavoaquino357@gmail.com>
2023-07-01 15:05:02 -0300

    feat:pesquisar-amazon

--- bot.py
+++ bot.py
@@ -1,8 +1,29 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+from OpenBrowser import OpenBrowser
+from lerXLSX import LerConfig
 import time
 import os
 import sys
 
+dictConfig = LerConfig()
+item = input("Qual item deseja pesquisar via Amazon?\n")
+driver = OpenBrowser("https://www.amazon.com.br")
+
+def pesquisarItem(item):
+    searchbar = driver.find_element(By.XPATH, '//*[@id="twotabsearchtextbox"]')
+    searchbar.send_keys(f"{item}")
+    searchbar.send_keys(Keys.ENTER)
+    prices = driver.find_elements(By.CLASS_NAME, 'a-price-whole')
+    fraction = driver.find_elements(By.CLASS_NAME, 'a-price-fraction')
+    pricesList = []
+    for i in range(5):
+        print(prices[3].text)
+        print(fraction[3].text)
+        #fullPrice = f'{prices[i].text},{fraction[i].text}'
+        #pricesList.append(fullPrice) 
+    return pricesList
+preco = pesquisarItem(item)
+print(preco)
 
