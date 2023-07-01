dce39defac3b228df34484fb6129b67b3dd10475
Lucas Jansen Gorges <lucasjnsgorges@gmail.com>
2023-07-01 15:50:55 -0300

    feat:click-lowest-price

--- bot.py
+++ bot.py
@@ -15,11 +15,26 @@ def pesquisarItem(item):
     searchbar = driver.find_element(By.XPATH, '//*[@id="twotabsearchtextbox"]')
     searchbar.send_keys(f"{item}")
     searchbar.send_keys(Keys.ENTER)
-    time.sleep(1)
+    time.sleep(5)
     prices = driver.find_elements(By.CLASS_NAME, 'a-price-whole')
     fraction = driver.find_elements(By.CLASS_NAME, 'a-price-fraction')
     pricesList = []
+    
     for i in range(5):
-        fullPrice = f'{prices[i].text},{fraction[i].text}'
-        pricesList.append(fullPrice) 
+        try:
+            price = prices[i].text
+            price = str(price).replace('.', '')
+        except:
+            pass
+        fullPrice = f'{price}.{fraction[i].text}'
+        pricesList.append(float(fullPrice)) 
     return pricesList
+
+def tratarDados(pricesList):
+    minValue = min(pricesList)
+    indexValue = pricesList.index(minValue)
+    titles = driver.find_elements(By.CSS_SELECTOR, 'h2.s-line-clamp-4')
+    titles[indexValue].click()
+
+lista = pesquisarItem(item)
+tratarDados(lista)
