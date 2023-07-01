777de901e3555e7e7481c0016f6d56f017bf72b0
GustavoAquino36 <gustavoaquino357@gmail.com>
2023-07-01 13:31:02 -0300

    fix:workflows

--- bot.py
+++ bot.py
@@ -1,22 +1,8 @@
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
-from openpyxl import load_workbook
 import time
 import os
 import sys
 
-def LerConfig():
-    # Func criada para ler linha a linha do settings.xlsx e adiciona-los em um dict
-    config = load_workbook(filename="settings.xlsx")
-    settings = config.get_sheet_by_name("Settings")
 
-    dicConfig = dict()
-
-    var_intTotalRows = settings.max_row
-    for i in range(1, var_intTotalRows):
-        key = settings["A" + (i+1).__str__()].value
-        value = settings["B" + (i+1).__str__()].value
-        if key != None and value != None: 
-            dicConfig[key] = value
-    return dicConfig
